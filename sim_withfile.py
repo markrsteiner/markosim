2f7b746cab66801fbe4c7d88879e0945ef99c5a8
marksteiner <markrsteiner@gmail.com>
2018-02-22 09:06:30 -0500

    added file ability. looks pretty good now.

--- sim_withfile.py
+++ sim_withfile.py
@@ -7,7 +7,7 @@ sim_tools.file_value_checker(file_values)
 m_sim_results = sim_tools.m_sim_runner(file_values, input_file_values)
 sim_tools.output_file_writer(m_sim_results)
 
-run_mine = False
+run_mine = True
 
 if run_mine:
     mark_sim_results = sim_tools.mark_sim_runner(file_values, input_file_values)
